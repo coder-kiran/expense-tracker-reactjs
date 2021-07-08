ccdde817ed242fda6868701a1803cca009cd14b0
Kiran K K <kirankalak4@gmail.com>
2021-07-08 14:20:11 +0530

    splitted component ExpenseItem into ExpenseDate component for styling calender

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
-import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 function ExpenseItem(props) {
-
   return (
     <div className="expense-item">
-      <div>{props.date}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-      <h2> {props.title}</h2> 
+        <h2> {props.title}</h2>
       </div>
-      <div className="expense-item__price">
-        $ {props.amount}
-      </div>     
+      <div className="expense-item__price">$ {props.amount}</div>
     </div>
   );
 }
