02c55e0449b3bccb566fabe6a3a45fca9f048e7d
Kiran K K <kirankalak4@gmail.com>
2021-07-11 17:13:29 +0530

    fixed some bugs

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -18,7 +18,6 @@ const ExpensesChart = (props) => {
     for(const expense of props.expenses){
         const expenseMonth = expense.date.getMonth();
         chartDataPoints[expenseMonth].value+= expense.amount;
-
     }
     
 return <Chart dataPoints={chartDataPoints} />
